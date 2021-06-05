a7c2a1a24cc12d779cec390879e5659243c7ed83
Madhuri <harshupatil0301@gmail>
2021-06-05 10:30:11 -0700

    Creating Contact

---
+++ AddressBook/ContactDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class ContactDetails
+    {
+        //private variables
+        private string firstname;
+        private string lastname;
+        private string address;
+        private string city;
+        private string state;
+        private long zip;
+        private long phoneNumber;
+        private string email;
+
+        //passing parameters
+        public ContactDetails(string firstname, string lastname, string address, string city, string state, long zip, long phoneNumber, string email)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        //to get and set the values
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public long Zip { get => zip; set => zip = value; }
+        public long PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+
+    }
+         
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,16 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Address Book System");
+
+            //user details
+            ContactDetails contactdetails = new ContactDetails("Harshu","Patil","GujarLane","Shahada","Maharashtra",425409,76872899,"harshupatil@gmail.com");
+            Console.WriteLine("Contact Detail is Created for : " + contactdetails.Firstname+ " " + contactdetails.Lastname);
+            Console.WriteLine("Address Is :" +contactdetails.Address);
+            Console.WriteLine("City Is : " + contactdetails.City);
+            Console.WriteLine("State Is : " + contactdetails.State);
+            Console.WriteLine("Zip Is : " + contactdetails.Zip);
+            Console.WriteLine("Phone Number Is : " + contactdetails.PhoneNumber);
+            Console.WriteLine("Email Is : " + contactdetails.Email);
         }
     }
 }
