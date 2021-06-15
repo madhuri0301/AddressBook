ace168d3ec5fd45c5cb0d42668d163c3e26f9441
Madhuri <harshupatil0301@gmail>
2021-06-15 17:15:15 +0530

    Adding Multiple Address Books

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,49 +6,69 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            
+            int adno;
             Console.WriteLine("Welcome To The Address Book System");
-            
-            AddressBookProgram AD = new AddressBookProgram();                    // Creating a Object of Main Class     
-            while (true)                                // Checking a Choice with Switch Statement
-            {
-                Console.WriteLine("1. Adding Contact \n2. Show All Contact \n3. Edit \n4.Delete \n5. Exit");
-                Console.WriteLine("Enter a Your Choice :");
-                int ch = Convert.ToInt32(Console.ReadLine());
 
-                switch (ch)
+
+            AddressBookProgram AD = new AddressBookProgram();                     // Creating a Object of Main Class
+            AddressBookProgram AD2 = new AddressBookProgram();
+            while (true)
+            {
+                Console.WriteLine("1. Personal Address Book \n 2. Shop Address Book");
+                Console.WriteLine("Select the Address Book:");
+                adno = Convert.ToInt32(Console.ReadLine());
+                switch (adno)
                 {
                     case 1:
-                        ContactDetails ContactDetails = AD.GetContactInput();
-                        AD.addContact(ContactDetails);
-
-                        Console.WriteLine("Contact Added Successfully");
+                        ManageADBOOK(AD);
                         break;
-
                     case 2:
-                        AD.ShowContactOutput();
-                        Console.WriteLine("Please Enter Your Information");
+                        ManageADBOOK(AD2);
                         break;
-
-                    case 3:
-                        AD.EditContact();
-                        //Console.WriteLine("Contact Edited Succesfully");
-                        break;
-
-                    case 4:
-                        AD.DeleteContact();
-                        break;
-
-                    case 5:
-                        Console.WriteLine("Thank You We are Exiting");
-                        return;
-
                     default:
-                        Console.WriteLine("Sorry You are Type Wrong Choice");
                         break;
-
                 }
             }
+        }
+        public static void ManageADBOOK(AddressBookProgram ABP)
+        {
+            //while (true)                                // Checking a Choice with Switch Statement
+            // {
+            Console.WriteLine("1. Adding Contact \n2. Show All Contact \n3. Edit \n4. Delete \n5. Exit");
+            Console.WriteLine("Enter a Your Choice :");
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 1:
+                    ContactDetails ContactDetails = ABP.GetContactInput();
+                    ABP.addContact(ContactDetails);
+
+                    Console.WriteLine("Contact Added Successfully");
+                    break;
+
+                case 2:
+                    ABP.ShowContactOutput();
+                    Console.WriteLine("Please Enter Your Information");
+                    break;
+
+                case 3:
+                    ABP.EditContact();
+                    //Console.WriteLine("Contact Edited Succesfully");
+                    break;
+                case 4:
+                    ABP.DeleteContact();
+                    break;
+                case 5:
+                    Console.WriteLine("Thank You We are Exiting");
+                    return;
+
+                default:
+                    Console.WriteLine("Sorry You are Type Wrong Choice");
+                    break;
+
+            }
+            //}
         }
     }
 }
